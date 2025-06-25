6a45564e8d1cf18fba897eb75203f23be0f2c5b5
Andrew Witten <awitten007@gmail.com>
2025-06-25 15:23:13 -0400

    does nothing yet

--- install-deps.sh
+++ install-deps.sh
@@ -10,8 +10,17 @@ install_velox_deps() {
     popd
 }
 
+install_apt() {
+    sudo apt update && sudo apt install -y libbenchmark-dev libgflags-dev
+}
+
+mkdir -p $PWD/deps-download
+mkdir -p $PWD/deps-install
+
 export DEPENDENCY_DIR=$PWD/deps-download
 export INSTALL_PREFIX=$PWD/deps-install
+
 install_velox_deps
+install_apt
 
 popd
--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 
 #include <iostream>
+#include "velox/buffer/Buffer.h"
+#include "velox/common/memory/Memory.h"
+
+namespace velox = facebook::velox;
 
 int main() {
-    std::cout << "asdf" << std::endl;
+    velox::memory::MemoryManager::Options options;
+    velox::memory::MemoryManager::initialize(options);
+    auto pool = velox::memory::memoryManager()->addLeafPool();
+
+    auto values = velox::AlignedBuffer::allocate<int64_t>(100, pool.get());
+    auto nulls = velox::AlignedBuffer::allocate<bool>(100, pool.get());
+
     return 0;
 }
